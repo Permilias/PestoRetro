d2921e2b1e528cf29703bbcaf239d7c6e45870e6
Julien <j.crepin@rubika-edu.com>
2020-04-27 18:08:00 +0200

    Structure AI

---
+++ Assets/Scripts/AI/Brute/BruteBrain.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // ############### VARIABLES ###############
+    public int life;
+    public Vector2 coord;
+    public Pasta pastaToLoot;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+
+    public int cacPower;
+    public int chargePower;
+    public int hitForCharge;
+
+    
+    private AICharacter brute;
+
+
+    // ############### FUNCTIONS ###############
+    private void Start()
+    {
+        brute = new AICharacter(life, coord, pastaToLoot, spriteLeft, cacPower, chargePower, hitForCharge, hitForCharge);
+    }
+
+    private void Update()
+    {
+        Look();
+
+        if (brute.HitRemaningForCharge <= 0)
+        {
+            Charge();
+        }
+        else
+        {
+            Reach();
+        }
+        
+    }
+
+    private void Look()
+    {
+        if (PlayerController.player.coord.x < brute.Coord.x)
+        {
+            brute.Sprite = spriteLeft;
+        }
+        else
+        {
+            brute.Sprite = spriteRight;
+        }
+    }
+
+    private void Reach()
+    {
+        // Mvt
+
+        if (true)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        PlayerController.player.life -= brute.CacPower;
+    }
+
+    private void Charge()
+    {
+        // Mvt (speed up) --> stop if this.x >/< player.x
+
+        // collision --> degat + projeter ?
+
+        // if mvt stop ou degat
+        if ()
+        {
+            brute.HitRemaningForCharge = brute.HitForCharge;
+        }
+    }
+
+    private void Dead()
+    {
+        Destroy(this);
+    }
+
+    public void TakeDamage(Pasta pasta)
+    {
+        brute.Life -= pasta.degats;
+        brute.HitRemaningForCharge--;
+    }
+}
---
+++ Assets/Scripts/AI/Soldado/SoldadoBrain.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class SoldadoBrain : MonoBehaviour
+{
+    // ############### VARIABLES ###############
+    public int life;
+    public Vector2 coord;
+    public Pasta pastaToLoot;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+
+    public int cooldown;
+    public Pasta pastaToShoot;
+
+
+    private float timer;
+    private AICharacter soldado;
+
+
+    // ############### FUNCTIONS ###############
+    private void Start()
+    {
+        soldado = new AICharacter(life, coord, pastaToLoot, spriteLeft, cooldown, pastaToShoot);
+        timer = cooldown;
+    }
+
+    private void Update()
+    {
+        Look();
+
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Shoot();
+            timer = soldado.Cooldown;
+        }
+    }
+
+    private void Look()
+    {
+        if (PlayerController.player.coord.x < soldado.Coord.x)
+        {
+            soldado.Sprite = spriteLeft;
+        }
+        else
+        {
+            soldado.Sprite = spriteRight;
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayerController.Shoot(PlayerController.player.coord, soldado.PastaToShoot);
+    }
+
+    private void Dead()
+    {
+        Destroy(this);
+    }
+
+    public void TakeDamage(Pasta pasta)
+    {
+        soldado.Life -= pasta.degats;
+    }
+}
