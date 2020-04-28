4812fb2c6292c9d1a6b6d257c157f59dee740bc8
TristanHome <t.veille@rubika-edu.com>
2020-04-28 17:15:29 +0200

    Pactole Done

--- Assets/Scripts/PactoleScript.cs
+++ Assets/Scripts/PactoleScript.cs
@@ -6,28 +6,79 @@ using TMPro;
 
 public class PactoleScript : MonoBehaviour
 {
-    bool canPickUp = false;
+    [SerializeField] private TextMeshProUGUI pickUpText;
+    [SerializeField] private Transform targetPoint;
 
-    public void EnterInTrigger ()
+    private bool pickUpAllowed;
+    private bool objectPickedUp;
+
+    private CharacterRaycaster raycast;
+
+    private void Start()
     {
-        canPickUp = true;
+        pickUpText.gameObject.SetActive(false);
+        objectPickedUp = false;
+
+        raycast = GetComponent<CharacterRaycaster>();
     }
 
     private void Update()
     {
-        if (canPickUp && Input.GetKeyDown(KeyCode.E))
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("is pick up");
+            PickUp();
+        }
+
+        else if (objectPickedUp == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Drop();     
+        }
+
+        if (!objectPickedUp)
+        {
+            Vector3 movement = new Vector3(0, -9.81f);
+            movement *= Time.deltaTime;
+            raycast.Move(movement);
+        }
+
+        Debug.Log(objectPickedUp);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
         }
     }
 
-    public void ExitInTrigger()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("you can't pick up");
+        if(collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    private void PickUp ()
     {
-        Debug.Log("hello");
+        Debug.Log("Pactole is picked up!");
+        objectPickedUp = true;
+
+        this.transform.position = targetPoint.transform.position;
+        this.transform.parent = targetPoint.transform;
+
+        PlayerController._instance.hasBag = true;
+    }
+
+    private void Drop ()
+    {
+        this.transform.parent = null;
+
+        objectPickedUp = false;
+
+        PlayerController._instance.hasBag = false;
     }
 }
