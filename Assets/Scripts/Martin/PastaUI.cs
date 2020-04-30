fbdde576908ff53da94ecf5c15231fc1d603ac20
Permilias <visseboulon.engrenaze@gmail.com>
2020-04-30 10:40:20 +0200

    PastaUI décommentée

--- Assets/Scripts/Martin/PastaUI.cs
+++ Assets/Scripts/Martin/PastaUI.cs
@@ -22,7 +22,7 @@ public class PastaUI : MonoBehaviour
     }
     private void Update()
     {
-      /*  for(int i = 0;i  < munitionsImage.Length; i++)
+       for(int i = 0;i  < munitionsImage.Length; i++)
         {
             munitionsImage[i].color = new Color(1, 1, 1, 0.2f);
 
@@ -48,6 +48,6 @@ public class PastaUI : MonoBehaviour
         }
 
 
-       */
+       
     }
 }
