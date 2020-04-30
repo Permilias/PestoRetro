b9f1da86649ca8a1c2c275b8abb1bf8d42016e02
Julien <j.crepin@rubika-edu.com>
2020-04-30 15:45:25 +0200

    IA Sound

--- Assets/Scripts/AI/Brute/BruteBrain.cs
+++ Assets/Scripts/AI/Brute/BruteBrain.cs
@@ -84,6 +84,9 @@ public class BruteBrain : MonoBehaviour
             {
                 bruteAnimator.SetBool("HasImpact", true);
                 bruteAnimator.SetBool("IsCharging", false);
+
+                SoundManager.Instance.PlaySound(SoundManager.Instance.enemyBrutePunch);
+
                 StartCoroutine("ImpactCharge");
 
                 raycaster.collisions.Reset();
@@ -94,6 +97,9 @@ public class BruteBrain : MonoBehaviour
             else
             {
                 bruteAnimator.SetBool("IsCharging", true);
+
+                SoundManager.Instance.PlaySound(SoundManager.Instance.enemyBruteCharge);
+
                 Charge();
             }
         }
@@ -271,6 +277,9 @@ public class BruteBrain : MonoBehaviour
                     if (brute.Life <= 0 && !bruteAnimator.GetBool("IsDead"))
                     {
                         bruteAnimator.SetBool("IsDead", true);
+
+                        SoundManager.Instance.PlaySound(SoundManager.Instance.enemyBruteDies);
+
                         StartCoroutine("Dead");
                     }
                 }
--- Assets/Scripts/AI/Soldado/SoldadoBrain.cs
+++ Assets/Scripts/AI/Soldado/SoldadoBrain.cs
@@ -68,6 +68,8 @@ public class SoldadoBrain : MonoBehaviour
                 {
                     soldadoAnimator.SetBool("IsShooting", true);
 
+                    SoundManager.Instance.PlaySound(SoundManager.Instance.enemySimpleShot);
+
                     StartCoroutine("Shoot");
                     timerReload = soldado.Cooldown;
                 }
@@ -116,7 +118,7 @@ public class SoldadoBrain : MonoBehaviour
                     timerImmobile = timeSpaghettiCookedStopIA;
                 }
 
-                SoundManager.Instance.PlaySound(SoundManager.Instance.enemySimpleHit);
+                SoundManager.Instance.PlaySound(SoundManager.Instance.enemySimpleHit);  
                 soldado.Life -= pastaProjectile.shotConfig.damage;
 
                 PastaManager.Instance.Repool(pastaProjectile);
@@ -124,6 +126,9 @@ public class SoldadoBrain : MonoBehaviour
                 if (soldado.Life <= 0 && !soldadoAnimator.GetBool("IsDead"))
                 {
                     soldadoAnimator.SetBool("IsDead", true);
+
+                    SoundManager.Instance.PlaySound(SoundManager.Instance.enemySimpleDies);
+
                     StartCoroutine("Dead");
                 }
             }
