463c406792bb1bc2bd8437cb4fe5f70701293b86
Permilias <visseboulon.engrenaze@gmail.com>
2020-04-30 16:49:14 +0200

    Spaghetti fix

--- Assets/Scripts/AI/Soldado/SoldadoBrain.cs
+++ Assets/Scripts/AI/Soldado/SoldadoBrain.cs
@@ -99,6 +99,7 @@ public class SoldadoBrain : MonoBehaviour
         PastaProjectile projectile = PastaManager.Instance.CreateProjectileAtPosition(PastaManager.Instance.pastas[shotPasta].config.crudeShot,
                                                                                     (Vector2)transform.position + offset, "IA", PastaManager.Instance.pastas[shotPasta]);
         projectile.SetDirection(facingLeft);
+        projectile.transform.eulerAngles = Vector3.zero;
         projectile.shotByPlayer = false;
         projectile.Shoot();
 
